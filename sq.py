b72b4e531f0c9d2cbffcede58f378674dd62df38
yoshiki-ota <football_18.y_o@icloud.com>
2022-02-12 16:39:41 +0900

    とりあえず戻した

--- sq.py
+++ sq.py
@@ -14,9 +14,11 @@ soup = BeautifulSoup(rest.text, 'lxml')  # BeautifulSoupを用いてlxmlで解�
 
 def i():
     for today_info1 in soup.find_all(href=re.compile(today)):
-        for today_info2 in today_info1.find_all(text=re.compile('横浜')):
+        for today_info2 in today_info1.find_all(text=re.compile('(江坂|横浜)')):
             title = today_info2
             url = today_info1.attrs['href']
             msg = f"[TITLE]:{title},[URL]: {url}"
             return msg
-    # return title, url
+    #         print(title)
+    #         print(url)
+    # # return title, url
