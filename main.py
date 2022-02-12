118258f10c0200ec316be1822a50d2bd40ee094b
yoshiki-ota <football_18.y_o@icloud.com>
2022-02-12 18:25:44 +0900

    絶対むり

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,
                                    TextSendMessage(text=xxx))
 
-    if __name__ == "__main__":
-        port = int(os.getenv("PORT", 5000))
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
